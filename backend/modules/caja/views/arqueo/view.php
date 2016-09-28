f5e8815d230315a1648bcf3cd566a82d08b167d7
Rafael Vences <rafael.vences@gmail.com>
2016-09-28 11:57:36 -0500

    - ENH - Realiza el arqueo diario

--- backend/modules/caja/views/arqueo/view.php
+++ backend/modules/caja/views/arqueo/view.php
@@ -6,8 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\caja\models\Arqueo */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Arqueos', 'url' => ['index']];
+$this->title = "Arqueo del día de " . $model->username;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="arqueo-view">
@@ -16,14 +15,56 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Cerrar', ['cerrar', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '¿ Estas seguro de cerrar el turno ?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
+
+
+
+    <div class="row">
+        <div class="col-sm-6">
+            Usuario: <?= $model->username ?>
+        </div>
+        <div class="col-sm-6">
+            Fecha del Arqueo: <?php $formatter = \Yii::$app->formatter;  echo $formatter->asDatetime($model->farqueo, 'long'); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            El sistema reporto una venta en efectivo de $<?= $model->efectivosistema ?> y de $<?= $model->dineroelectronico ?> con tarjetas o créditos
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
+            Compras o pagos realizados en efectivo: $<?= $model->egresocompras + $model->egresocomprasservicio ?>
+        </div>
+        <div class="col-sm-4">
+            Retiro de Excedente en caja fue de: $<?= $model->retiroempresa ?>
+        </div>
+        <div class="col-sm-4">
+            Depósitos por falta de efectivo fue de: $<?= $model->depositoempresa ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            El turno se abrió con $<?= $model->efectivoapertura ?> y cerro con $<?= $model->efectivocierre ?> en efectivo, y debió cerrar con: $<?=$model->efectivofisico ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            El adeudo anterior <?= $model->adeudoanterior ?> sumado con el adeudo actual <?= $model->adeudoactual - $model->adeudoanterior?> al cierre del día es de <b>$<?= $model->adeudoactual    ?></b>
+        </div>
+    </div>
+<?php /*
 
     <?= DetailView::widget([
         'model' => $model,
@@ -46,7 +87,8 @@ $this->params['breadcrumbs'][] = $this->title;
             'adeudoactual',
             'ventaturno',
             'egresoturno',
+            'cerrado',
         ],
     ]) ?>
-
+*/ ?>
 </div>
